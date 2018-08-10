e30bb80c3a6f71245d805697a10a66e313d7149b
Shakeria Smith <shakerias2@gmail.com>
2018-08-10 15:08:57 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
+require 'pry'
+
 def unsafe?(speed)
-
+	if speed < 40
+		return true
+	elsif speed > 60
+		return true
+	else
+		return false
 end
-
-
+end
 
 def not_safe?(speed)
-	
+	binding.pry
+	speed ==  40-60 ? "true" : "false"
 end
-	
-
-
